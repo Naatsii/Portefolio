1cf241cadfbf5e71a395868291bc9e24339d92c2
Naatsii <tristan.coulaud@hotmail.fr>
2026-01-18 15:39:42 +0100

    18/01 15h39

--- js/app.js
+++ js/app.js
@@ -7,3 +7,28 @@ window.addEventListener('scroll', () =>{
         monHeader.classList.remove('header-active');
     }
 });
+
+const openMenu = () => {
+    const menu = document.querySelector(".header-menu");
+    const icon = document.querySelector(".header-menu-mobile .material-icons")
+
+    menu.classList.toggle("active");
+
+    if(menu.classList.contains("active")){
+        icon.innerHTML = "close";
+        document.body.style.overflow = "hidden";
+    } else {
+        icon.innerHTML = "menu";
+        document.body.style.overflow = "auto";
+    }
+}
+
+const links = document.querySelectorAll(".header-menu a");
+links.forEach(link => {
+    link.addEventListener("click", () => {
+        const menu = document.querySelector(".header-menu");
+        if(menu.classList.contains("active")) {
+            openMenu();
+        }
+    });
+});
